9223a8ee4dc4ff058f4e9d07e43235279c4abbd3
Elias <eliasventura23@gmail.com>
2023-02-02 20:25:30 -0500

    finished lab

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,15 @@
+class StudentsController < ApplicationController
+
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        students_by_grades = Student.order(grade: :desc)
+        render json: students_by_grades
+    end
+
+    def highest_grade
+        render json: Student.maximum(:grade)
+    end
+end
